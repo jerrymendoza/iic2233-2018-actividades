a613936c3872bb8de64fe12869848ba7733971a7
Jerry Mendoza <jamendoza@uc.cl>
2018-03-15 15:57:17 -0300

    intento de clases

--- Actividades/AC01/a.py
+++ Actividades/AC01/a.py
@@ -1,14 +1,48 @@
+import random
 class Persona:
-	rut=0
-	def __init__(self, nombre, fecha_nacimiento):
-		self.rut = Persona.rut
-		Persona.rut +=1
-		self.nombre = nombre
-		self.f_nacimiento=date(fecha_nacimiento) #dia/mes/anno
+    rut=0
+    def __init__(self, nombre, fecha_nacimiento):
+        self.rut = Persona.rut
+        Persona.rut +=1
+        self.nombre = nombre
+        self.f_nacimiento=date(fecha_nacimiento) #dia/mes/anno
 
 	def edad(self):
 
 
-class Alumno(Persona):
+        return str(0)
 
+    def __str__(self):
+        print(self.nombre+" "+self.edad())
+    
+
+class Alumno(Persona):
+    def __init__(self):
+        nivel_conocimiento = 10
+        ramos=[]
+
+    def estudiar(self):
+        nivel_conocimiento += random.randrange(5,10)
+        
+
+class Profesor(Persona):
+    def __init__(self,seccion):
+        self.seccion = seccion
+
+	def ensennar():
+        
+        pass
+
+    def __str__(self):
+        print(self.nombre+" "+self.edad())
+
+class Ayudante(Alumno):
+    def __init__(self):
+
+
+    def ensennar():
+        pass
+
+    def __str__(self):
+        print(self.nombre+" "+self.edad())
 
