186501ae647174314ab64ab0ecd52c71d55aeda1
Jerry Mendoza <jamendoza@uc.cl>
2018-03-15 15:29:41 -0300

    commit inicial

---
+++ Actividades/AC01/a.py
@@ -0,0 +1,14 @@
+class Persona:
+	rut=0
+	def __init__(self, nombre, fecha_nacimiento):
+		self.rut = Persona.rut
+		Persona.rut +=1
+		self.nombre = nombre
+		self.f_nacimiento=date(fecha_nacimiento) #dia/mes/anno
+
+	def edad(self):
+
+
+class Alumno(Persona):
+
+
