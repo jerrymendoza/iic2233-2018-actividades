7b65714bc2c5008a12f9eefc8c193c9269d4db97
Jerry Mendoza <jamendoza@uc.cl>
2018-04-07 20:42:48 -0300

    kwards para las clases

--- Tareas/T01/main.py
+++ Tareas/T01/main.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 from lib import read_csv 
 from misclases import Galaxia,Maestro,Aprendiz,Asesino
 
 
-planeta1 = Maestro("uno")
+planeta1 = Maestro("uno",tasa_minerales=20,soldados=30)
 print(planeta1.soldados)
 planeta1.soldados = 10
 print(planeta1.soldados)
@@ -11,21 +10,29 @@ planeta1.magos = 101
 print(planeta1.magos)
 planeta1.magos = 90
 print(planeta1.magos)
-
-
-
+print("tasa:")
+print(planeta1.tasa_minerales)
 
 
 #print(read_csv("planetas.csv","planeta"))
 
 #print(read_csv("galaxias.csv","galaxia"))
+galaxias=[]
 
+print("{} ChauCraft {}".format("*"*20,"*"*20).center(80))
 
-print("{} GALAXIANEITOR 3000 {}".format("*"*20,"*"*20).center(80))
-
+print("     (0) Cargar Archivos csv")
 print("     (1) Crear Galaxia")
 print("     (2) Modificar una Galaxia")
 print("     (3) Consultar sobre una Galaxia")
 print("     (4) Jugar en una Galaxia")
 
 input("\nOpcion: ")
+
+print("(1) Crear Galaxia =>")
+nombre_galaxia=input("Nombre para la Galaxia nueva: ")
+
+galaxias.append(Galaxia(nombre_galaxia))
+
+for i in galaxias:
+    print(i)
--- Tareas/T01/misclases.py
+++ Tareas/T01/misclases.py
@@ -1,12 +1,16 @@
+import random
+from datetime import datetime
 class Galaxia:
     """docstring for Galaxia"""
-    def __init__(self, nombre):
+    def __init__(self, nombre,**kwargs):
         self.nombre = nombre.lower()
         self.planetas = []
+        self.minerales = kwargs.get('minerales',0)
+        self.deuterio = kwargs.get('deuterio',0)
     def __repr__(self):
         s = "***** {} *****\n".format(self.nombre.upper())
-        if len(planetas)>0:
-            for i in planetas:
+        if len(self.planetas)>0:
+            for i in self.planetas:
                 s+="- {} \n".format(i.nombre.capitalize())
         else:
             s+="Esta Galaxia no tiene planetas"
@@ -15,16 +19,16 @@ class Galaxia:
 
 class Planeta:
     """docstring for Planeta"""
-    def __init__(self, nombre):
+    def __init__(self, nombre,**kwargs):
         self.nombre = nombre.lower()
-        self.tasa_minerales = 0 #tasa minerales x segundo
-        self.tasa_deuterio = 0 #tasa deuterio x segundo
-        self.cuartel = False
-        self.torre = False
-        self.ultima_recoleccion = 56676 #ultima recoleccion
-        self.__nivel_ataque = 0
-        self.__nivel_economia = 0
-        self.conquistado = False
+        self.tasa_minerales = kwargs.get('tasa_minerales') #tasa minerales x segundo
+        self.tasa_deuterio = kwargs.get('tasa_deuterio',0)
+        self.cuartel = kwargs.get('cuartel',False)
+        self.torre = kwargs.get('torre',False)
+        self.ultima_recoleccion = kwargs.get('ultima_recoleccion',datetime.now())
+        self.__nivel_ataque = kwargs.get('nivel_ataque',0) 
+        self.__nivel_economia = kwargs.get('nivel_economia',0)
+        self.conquistado = kwargs.get('conquistado',False)
         self.__evolucion = 0
 
 
@@ -54,18 +58,18 @@ class Planeta:
 
 class Maestro(Planeta):
     """docstring for Maestro"""
-    def __init__(self, nombre):
-        super().__init__(nombre)
+    def __init__(self, nombre,**kwargs):
+        super().__init__(nombre,**kwargs)
         self.raza = "maestro"
         self.poblacion_max = 100
-        self.__soldados = 0 
-        self.__magos = 0
-        self.__costo_soldado = 0
-        self.__costo_mago = 0
-        self.__ataque_soldado = 0
-        self.__ataque_mago = 0
-        self.__vida_soldado = 0
-        self.__vida_mago = 0
+        self.__soldados = kwargs.get('soldados',0) 
+        self.__magos = kwargs.get('magos',0) 
+        self.__costo_soldado = (200,300)
+        self.__costo_mago = (300,400)
+        self.__ataque_soldado = random.randint(60, 80)
+        self.__ataque_mago = random.randint(80, 120)
+        self.__vida_soldado = random.randint(200, 250)
+        self.__vida_mago = random.randint(150, 200)
         self.grito = "¡Nuestro conocimiento nos ha otorgado uan victoria más!"
 
     def __repr__(self):
@@ -78,7 +82,7 @@ class Maestro(Planeta):
     @soldados.setter
     def soldados(self, p):
         if p + self.magos <= self.poblacion_max:
-            self.__soldados = p
+            self.__soldados += p
 
     @property
     def magos(self):
@@ -87,18 +91,18 @@ class Maestro(Planeta):
     @magos.setter
     def magos(self,p):
         if self.soldados + p <= self.poblacion_max:
-            self.__magos = p
+            self.__magos += p
 
 class Aprendiz(Planeta):
     """docstring for Aprendiz"""
-    def __init__(self, nombre):
-        super().__init__(nombre)
+    def __init__(self, nombre,**kwargs):
+        super().__init__(nombre, **kwargs)
         self.raza = "aprendiz"
         self.poblacion_max = 150
-        self.__soldados = 0 
-        self.__costo_soldado = 0
-        self.__ataque_soldado = 0
-        self.__vida_soldado = 0
+        self.__soldados = kwargs.get('soldados',0) 
+        self.__costo_soldado = (300,400)
+        self.__ataque_soldado = random.randint(30, 60)
+        self.__vida_soldado = random.randint(600, 700)
         self.grito = "¡Con una gran defensa y medicinas, nuestros soldados " \
                      "son invencibles!"        
 
@@ -114,14 +118,14 @@ class Aprendiz(Planeta):
 
 class Asesino(Planeta):
     """docstring for Asesino"""
-    def __init__(self, nombre):
-        super().__init__(nombre)
+    def __init__(self, nombre, **kwargs):
+        super().__init__(nombre, **kwargs)
         self.raza = "asesino"
         self.poblacion_max = 400
-        self.__soldados = 0 
-        self.__costo_soldado = 0
-        self.__ataque_soldado = 0
-        self.__vida_soldado = 0
+        self.__soldados = kwargs.get('soldados',0) 
+        self.__costo_soldado = (100,200)
+        self.__ataque_soldado = random.randint(40, 45)
+        self.__vida_soldado = random.randint(250, 270)
         self.grito = "¡El poder de las sombras es lo unico necesario para " \
                      "ganar estas batallas!"
 
