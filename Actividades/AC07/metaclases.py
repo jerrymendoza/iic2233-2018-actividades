baa8847cd5888853550dd6c50bcc815bba475043
Jerry Mendoza <jamendoza@uc.cl>
2018-05-03 16:33:17 -0300

    fin

--- Actividades/AC07/metaclases.py
+++ Actividades/AC07/metaclases.py
@@ -2,27 +2,27 @@ import funciones
 
 
 class MetaAuto(type):
-    __contador=0
+    contador=0
     def __new__(meta, nombre, base_clases, diccionario):
         diccionario["piezas"]=funciones.crear_piezas()
         diccionario["definir_estado_piezas"]=funciones.definir_estado_piezas
-        #diccionario["contador"]=Meta.contador
         return super().__new__(meta, nombre, base_clases, diccionario)
         
     
     def __init__(cls, nombre, base_clases, diccionario):
-        #diccionario["contador"]=0
+        cls.contador=0
+        print(cls.contador)
         return super().__init__(nombre, base_clases, diccionario)
 
 
-    #def __call__(cls, *args, **kw):
-    #    if cls.contador<3:
-    #        cls.contador=+1
-    #        cls.instance = super().__call__(*args, **kw)
-    #        print(cls.contador)
-    #        return cls
-    #    else:
-    #        return None
+    def __call__(cls, *args, **kw):
+        if cls.contador<3:  
+            cls.contador+=1 
+            print(cls.contador)
+            cls.instance = super().__call__(*args, **kw)
+            return cls.instance
+        else:
+            return None
 
 class MetaTrabajador(type):
     def __new__(meta, nombre, base_clases, diccionario):
